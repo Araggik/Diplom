076db64856276f9e3d43d750a90de952523c45e8
Araggik <45770047+Araggik@users.noreply.github.com>
2021-05-29 18:06:44 +0500

    Add files via upload

---
+++ Modifications/FormsElements/Idef_ItemMenu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TriadNSim.Modifications.FormsElements
+{
+    public partial class Idef_ItemMenu : ToolStripMenuItem//UserControl
+    {
+        public void TestEvent(object sender, EventArgs e)
+        {
+           
+        }
+
+        public Idef_ItemMenu()
+        {
+            InitializeComponent();
+            this.Text = "IDEF";
+
+            ToolStripItem item1 = new ToolStripMenuItem();
+            item1.Text = "Translate IDEF";
+
+            this.DropDownItems.Add(item1);
+
+            ToolStripItem item2 = new ToolStripMenuItem();
+            item2.Text = "Create rules";
+
+            this.DropDownItems.Add(item2);            
+        }
+    }
+}
